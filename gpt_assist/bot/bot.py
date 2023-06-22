e65e15937b21f483b084fd492055b359ca527b81
Rafayel1992 <123087885+Rafayel1992@users.noreply.github.com>
2023-06-22 15:57:42 +0400

    bot now 2

--- gpt_assist/bot/bot.py
+++ gpt_assist/bot/bot.py
@@ -6,34 +6,29 @@ from gpt_assist.инструменти import inline
 bot = telebot.TeleBot(configure.config['token'])
 
 
-
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-
     keyboard = types.ReplyKeyboardMarkup(row_width=2)
-
 
     button1 = types.KeyboardButton("Снять квартиру")
     button2 = types.KeyboardButton('Аренда посуточно')
+    button3 = types.KeyboardButton('dlya branirovanya najmite suda')
 
-
-    keyboard.add(button1, button2)
-
+    keyboard.add(button1, button2, button3)
 
     bot.reply_to(message, "Привет! Я бот. "
-                                                                    "\n.Այստեղ դուկ կարողեք վարձակալել բնակարան"
-                                                                    "\n.Գնել  բնակարան"
-                                                                    "\n.Ինչպես նաև վարձակալել օրավարձով:\n"
-                                                                    "\n.Здесь можно снять квартиру."
-                                                                    "\n.Купить квартиру."
-                                                                    "\n.A так же аренда посуточно.\n"
-                                                                    '\n.Here you can rent an apartment.'
-                                                                    "\n.Buy an apartment as well as daily", reply_markup=keyboard)
+                          "\n.Այստեղ դուկ կարողեք վարձակալել բնակարան"
+                          "\n.Գնել  բնակարան"
+                          "\n.Ինչպես նաև վարձակալել օրավարձով:\n"
+                          "\n.Здесь можно снять квартиру."
+                          "\n.Купить квартиру."
+                          "\n.A так же аренда посуточно.\n"
+                          '\n.Here you can rent an apartment.'
+                          "\n.Buy an apartment as well as daily", reply_markup=keyboard)
 
 
 @bot.message_handler(func=lambda message: message.text == "Снять квартиру")
 def handle_button1(message):
-
     keyboard = types.InlineKeyboardMarkup()
 
     button1 = types.InlineKeyboardButton("Кнопка 1", callback_data='button1')
@@ -43,20 +38,19 @@ def handle_button1(message):
     keyboard.add(button1, button2, button3)
     bot.reply_to(message, "Привет! Выберите одну из кнопок:", reply_markup=keyboard)
 
+
 @bot.callback_query_handler(func=lambda call: True)
 def handle_inline_buttons(call):
-        if call.data == 'button1':
-            bot.send_message(call.message.chat.id, "Вы нажали на Кнопку 1")
-        elif call.data == 'button2':
-            bot.send_message(call.message.chat.id, "Вы нажали на Кнопку 2")
-        elif call.data == 'button3':
-            bot.send_message(call.message.chat.id, "Вы нажали на Кнопку 3")
+    if call.data == 'button1':
+        bot.send_message(call.message.chat.id, "Вы нажали на Кнопку 1")
+    elif call.data == 'button2':
+        bot.send_message(call.message.chat.id, "Вы нажали на Кнопку 2")
+    elif call.data == 'button3':
+        bot.send_message(call.message.chat.id, "Вы нажали на Кнопку 3")
 
 
 @bot.message_handler(func=lambda message: message.text == "Аренда посуточно")
 def handle_button2(message):
-
-
     key_board = types.InlineKeyboardMarkup()
 
     button1 = types.InlineKeyboardButton("Кнопка 1", callback_data='button1')
@@ -75,6 +69,14 @@ def handle_inline_buttons(call):
         bot.send_message(call.message.chat.id, "Вы нажали на Кнопку 2")
     elif call.data == 'button3':
         bot.send_message(call.message.chat.id, "Вы нажали на Кнопку 3")
+
+
+@bot.message_handler(func=lambda message: message.text == "dlya branirovanya najmite suda")
+def handle_button3(message):
+    bot.reply_to(message,
+                 'Nomer Telefona (+37498772389)'
+                 '\n email pochta \n {rafayel1624@gmail.com}'
+                 '\n Sprashovat \n Rafayel Hovhannisyan')
 
 
 bot.polling()
