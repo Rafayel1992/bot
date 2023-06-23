06f920546abf42639d3acc0affde1c3ef7c7c965
Rafayel1992 <123087885+Rafayel1992@users.noreply.github.com>
2023-06-23 17:08:34 +0400

    bot now 3

--- Bot/bot/bot.py
+++ Bot/bot/bot.py
@@ -1,82 +1,103 @@
+import random
 import telebot
-from Bot.инструменти import configure
+import webbrowser
 from telebot import types
+from Bot.инструменти import configure
 
 
 bot = telebot.TeleBot(configure.config['token'])
 
+answers = ['Я не понял, что ты хочешь сказать.', 'Извини, я тебя не понимаю.', 'Я не знаю такой команды.', 'Мой разработчик не говорил, что отвечать в такой ситуации... >_<']
 
+# Обработка команды /start
 @bot.message_handler(commands=['start'])
-def handle_start(message):
-    keyboard = types.ReplyKeyboardMarkup(row_width=1)
-
-    button1 = types.KeyboardButton("Снять квартиру")
-    button2 = types.KeyboardButton('Аренда посуточно')
-    button3 = types.KeyboardButton('dlya branirovanya najmite suda')
-
-    keyboard.add(button1, button2, button3)
-
-    bot.reply_to(message, "Привет! Я бот. "
-                          "\n.Այստեղ դուկ կարողեք վարձակալել բնակարան"
-                          "\n.Գնել  բնակարան"
-                          "\n.Ինչպես նաև վարձակալել օրավարձով:\n"
-                          "\n.Здесь можно снять квартиру."
-                          "\n.Купить квартиру."
-                          "\n.A так же аренда посуточно.\n"
-                          '\n.Here you can rent an apartment.'
-                          "\n.Buy an apartment as well as daily", reply_markup=keyboard)
+def welcome(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.KeyboardButton('🏠  Снять квартиру')
+    button2 = types.KeyboardButton('🏠  Аренда посуточно')
 
 
-@bot.message_handler(func=lambda message: message.text == "Снять квартиру")
-def handle_button1(message):
-    keyboard = types.InlineKeyboardMarkup(row_width=1)
+    markup.row(button1)
+    markup.row(button2)
 
-    button1 = types.InlineKeyboardButton("Кнопка 1", callback_data='button1')
-    button2 = types.InlineKeyboardButton("Кнопка 2", callback_data='button2')
-    button3 = types.InlineKeyboardButton("Кнопка 3", callback_data='button3')
+    if message.text == '/start':
 
-    keyboard.add(button1, button2, button3)
-    bot.reply_to(message, "Привет! Выберите одну из кнопок:", reply_markup=keyboard)
+        bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}!\nУ меня ты сможешь купить некоторые товары!\nКонтакт моего разработчика: https://t.me/Rafo1212', reply_markup=markup)
+    else:
+        bot.send_message(message.chat.id, 'Перекинул тебя в главном меню! Выбирай!', reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def handle_inline_buttons(call):
-    if call.data == 'button1':
-        bot.send_message(call.message.chat.id, "Вы нажали на Кнопку 1")
-    elif call.data == 'button2':
-        bot.send_message(call.message.chat.id, "Вы нажали на Кнопку 2")
-    elif call.data == 'button3':
-        bot.send_message(call.message.chat.id, "Вы нажали на Кнопку 3")
+@bot.message_handler(content_types='photo')
+def get_photo(message):
+    bot.send_message(message.chat.id, 'У меня нет возможности просматривать фото :(')
 
 
-@bot.message_handler(func=lambda message: message.text == "Аренда посуточно")
-def handle_button2(message):
-    key_board = types.InlineKeyboardMarkup(row_width=1)
+@bot.message_handler()
+def info(message):
+    if message.text == '🏠  Снять квартиру':
+        goodsChapter(message)
+    elif message.text == '🏠  Аренда посуточно':
+        settingsChapter(message)
+    elif message.text == '🔹 Товар #1':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.KeyboardButton('💳 Купить')
+        button2 = types.KeyboardButton('↩️ Назад')
+        markup.row(button1, button2)
+        bot.send_message(message.chat.id, 'Информация о первом товаре...', reply_markup=markup)
+    elif message.text == '🔹 Товар #2':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.KeyboardButton('💳 Купить')
+        button2 = types.KeyboardButton('↩️ Назад')
+        markup.row(button1, button2)
+        bot.send_message(message.chat.id, 'Информация о втором товаре...', reply_markup=markup)
+    elif message.text == '🔹 Товар #3':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.KeyboardButton('💳 Купить')
+        button2 = types.KeyboardButton('↩️ Назад')
+        markup.row(button1, button2)
+        bot.send_message(message.chat.id, 'Информация о третьем товаре...', reply_markup=markup)
+    elif message.text == '🔹 Товар #4':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.KeyboardButton('💳 Купить')
+        button2 = types.KeyboardButton('↩️ Назад')
+        markup.row(button1, button2)
 
-    button1 = types.InlineKeyboardButton("Кнопка 1", callback_data='button1')
-    button2 = types.InlineKeyboardButton("Кнопка 2", callback_data='button2')
-    button3 = types.InlineKeyboardButton("Кнопка 3", callback_data='button3')
+    elif message.text == '💳 Купить' or message.text == '✏️ Написать разработчику':
 
-    key_board.add(button1, button2, button3)
-    bot.reply_to(message, "Привет! Выберите одну из кнопок:", reply_markup=key_board)
+        webbrowser.open('https://t.me/Rafo1212')
+    elif message.text == '↩️ Назад':
+        goodsChapter(message)
+    elif message.text == '↩️ Назад в меню':
+        welcome(message)
 
-
-@bot.callback_query_handler(func=lambda call: True)
-def handle_inline_buttons(call):
-    if call.data == 'button1':
-        bot.send_message(call.message.chat.id, "Вы нажали на Кнопку 1")
-    elif call.data == 'button2':
-        bot.send_message(call.message.chat.id, "Вы нажали на Кнопку 2")
-    elif call.data == 'button3':
-        bot.send_message(call.message.chat.id, "Вы нажали на Кнопку 3")
+    else:
+        bot.send_message(message.chat.id, answers[random.randint(0, 3)])
 
 
-@bot.message_handler(func=lambda message: message.text == "dlya branirovanya najmite suda")
-def handle_button3(message):
-    bot.reply_to(message,
-                 'Nomer Telefona (+37498772389)'
-                 '\n email pochta \n {rafayel1624@gmail.com}'
-                 '\n Sprashovat \n Rafayel Hovhannisyan')
+def goodsChapter(message):
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.KeyboardButton('🔹 Товар #1')
+    button2 = types.KeyboardButton('🔹 Товар #2')
+    button3 = types.KeyboardButton('🔹 Товар #3')
+    button4 = types.KeyboardButton('🔹 Товар #4')
+    button5 = types.KeyboardButton('↩️ Назад в меню')
+    markup.row(button1, button2)
+    markup.row(button3, button4)
+    markup.row(button5)
 
 
-bot.polling()
+    bot.send_message(message.chat.id, 'Вот все товары, которые сейчас находятся в продаже:', reply_markup=markup)
+
+
+def settingsChapter(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.KeyboardButton('⚙️ Настройки #1')
+    button2 = types.KeyboardButton('⚙️ Настройки #2')
+    button3 = types.KeyboardButton('↩️ Назад в меню')
+    markup.row(button1, button2)
+    markup.row(button3)
+    bot.send_message(message.chat.id, 'Раздел настроек.\nВыбери один из вариантов:', reply_markup=markup)
+
+
+bot.polling(none_stop=True)
